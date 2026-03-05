d1964e78a6a2eedb2038102794f8211827d91ee5
chowkat75 <chowkat756@gmail.com>
2026-03-05 23:41:39 +0600

    Update Slider.jsx

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,8 +1,11 @@
-import banner from 'https://i.ibb.co.com/4nx0V3kW/Gemini-Generated-Image-6no43d6no43d6no4.png';
 const Banner = () => {
   return (
     <div className="w-full">
-      <img src={banner} alt="Banner" className="w-full h-auto object-cover" />
+      <img
+        src="https://i.ibb.co/4nx0V3kW/Gemini-Generated-Image-6no43d6no43d6no4.png"
+        alt="Banner"
+        className="w-full h-auto object-cover"
+      />
     </div>
   );
 };
